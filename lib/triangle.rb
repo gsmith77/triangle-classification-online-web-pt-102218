ecb0f84222edcc27f9a23147e1a8094ba8356a93
grant smith <grantsmith1256@gmail.com>
2018-11-30 19:02:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,12 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :equilateral, :isosceles, :scalene, :side1, :side2, :side3
+  
+  
+  def initialize(side1:, side2:, side3:)
+    if side1 == side2 && side2 == side3 && side1 == side3
+    return "equilateral"
+  end
+  end
+  
 end
