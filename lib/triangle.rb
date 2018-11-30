48ef3dcc2287f3353ba94888db93d691d23be706
grant smith <grantsmith1256@gmail.com>
2018-11-30 19:35:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
+require "pry"
+
 class Triangle
-  # write code here
+  
+  attr_accessor :equilateral, :isosceles, :scalene, :side, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+   
+  def kind
+    if @side1 == @side2 && @side3 != @side1
+      :isosceles
+    elsif (@side1 == @side2 && @side2 == @side3) && @side3 == @side1
+      :equilateral
+    end
+  end
+
+  
 end
